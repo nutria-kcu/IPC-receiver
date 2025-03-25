e91c690b286bc616a5d3ac18ea85d3f3253a0518
GeonhoRoh <roh517@naver.com>
2025-03-24 19:21:02 -0500

    TEST: SAMPLE IPC CONSUMER

--- IPC-receiver/IPC-receiver.cpp
+++ IPC-receiver/IPC-receiver.cpp
@@ -1,20 +1,44 @@
-// IPC-receiver.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Windows.h>
+#include <conio.h> // For _getch()
+#include "SharedMemory.h"
+#include <tchar.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#pragma comment(lib, "./lib/SharedMemory.lib")
+
+using namespace std;
+
+
+
+int main() {
+    char ch;
+    cout << "Press 'q' to print 'Welcome message'.\n";
+
+    SharedMemoryHandler* sh = new SharedMemoryHandler(_T("NUTRI-IPC"), 0);
+    HANDLE fullEvent = sh->getFullEvent();
+    HANDLE emptyEvent = sh->getEmptyEvent();
+
+
+    while (true) {
+        DWORD result = WaitForSingleObject(fullEvent, INFINITE);
+
+        SharedMemory* sm = sh->getMessage();
+
+        int cmd = sm->cmd;
+        int option = sm->option;
+
+        cout << "SetMSG with cmd: " << cmd << ", option: " << option << endl;
+
+
+        ResetEvent(fullEvent);
+        SetEvent(emptyEvent);
+
+        if (cmd == 0) {
+            break;
+        }
+
+    }
+    delete sh;
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
